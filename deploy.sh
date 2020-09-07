e9b5fa1315d7751d1b372eeec5df7fb76b4217ea
ZhangS3344 <zhangsenzhou@163.com>
2020-09-07 16:26:54 +0800

    first commit

--- deploy.sh
+++ deploy.sh
@@ -5,20 +5,18 @@ set -e
 
 # 生成静态文件
 # vuepress build --dest ./public
-npm run build
+npm run docs:build
 
 # 进入生成的文件夹
 cd docs/.vuepress/dist
-
-# 如果是发布到自定义域名
-# echo 'blog.gusibi.mobi' > CNAME
 
 git init
 git add -A
 git commit -m 'first commit'
 
 # 发布到 https://<USERNAME>.github.io
-git push -f git@github.com:BeautifulGirl230/MyVueproess.git master:gh-pages
+# git push -f git@github.com:BeautifulGirl230/MyVueproess.git master:gh-pages
+git push -f https://github.com/BeautifulGirl230/MyVueproess.git
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
